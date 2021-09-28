d0c42b1d7f9130a315dc15209524c1f958bd280a
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-09-29 01:42:50 +0530

    Create 4*4 grid

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // What is DOM Content Loaded?
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed,
-document.addEventListener("DOMContentLoader",()=>{
+document.addEventListener("DOMContentLoaded",()=>{
     const gridDisplay = document.querySelector(".grid");
+    const scoreCont = document.querySelector(".score-container");
+    const scoreDisplay = document.querySelector(".score");
+    let squares = [];
+    let width = 4;
+    function createGrid(){
+        for(let  i = 0; i < width*width ;i++){
+            let square = document.createElement('div');
+            square.innerHTML = 0;
+            square.classList.add("small-square")
+            gridDisplay.append(square);
+            squares.push(square);
+        }
+    }
+    createGrid();
+
 })
