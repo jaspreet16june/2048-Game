682b80036f2ff2a7ec36979e24425aed071d20be
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-09-28 23:57:01 +0530

    Grid

---
+++ script.js
@@ -0,0 +1,5 @@
+// What is DOM Content Loaded?
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed,
+document.addEventListener("DOMContentLoader",()=>{
+    const gridDisplay = document.querySelector(".grid");
+})
